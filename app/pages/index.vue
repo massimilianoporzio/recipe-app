6304c7d412dc0e1634e61430f7f61db5bfb6537d
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-28 16:14:02 +0200

    prova husky

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,7 +27,10 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
     <h1 :style="{ color }">
       {{ runtimeConfig.public.helloText }}
     </h1>
-    <NuxtLink to="/" external> refresh </NuxtLink>
+    <NuxtLink
+      to="/"
+      external
+    > refresh </NuxtLink>
   </div>
 </template>
 <!--
