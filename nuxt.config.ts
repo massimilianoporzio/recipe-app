0d8806c2fd85eb64fd909cb55048bc91f895b222
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-29 10:44:45 +0200

    added modules and taiwind on start page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/eslint'
-  ],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxt/icon', '@nuxt/image'],
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
